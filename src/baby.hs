9182a36e0f39b426adad58eb0c774c57e0b3f2ac
tykowale <tykowale@gmail.com>
2016-08-15 19:05:16 -0500

    first chapter

--- src/baby.hs
+++ src/baby.hs
@@ -32,3 +32,18 @@ lenFiver = length [5,4,3,2,1]
 
 checker = null [1,2,3] -- false
 checkerTrue = null [] -- true
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _  <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]
+
+rightTriangles =
+    [ (a,b,c) |
+    c <- [1..10],
+    b <- [1..c],
+    a <- [1..b],
+    a^2 + b^2 == c^2]
