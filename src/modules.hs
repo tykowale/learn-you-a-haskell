289c6e228caaac855c860996266ee2fb81147f7c
tykowale <tykowale@gmail.com>
2016-08-25 18:27:30 -0500

    bring in containers for Data.Map

---
+++ src/modules.hs
@@ -0,0 +1,40 @@
+import Data.List
+import Data.Char
+import qualified Data.Map as Map
+
+phoneBook :: Map.Map String String
+phoneBook = Map.fromList
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+
+wordNums :: String -> [(String, Int)]
+wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+encode :: Int -> String -> String
+encode offset = map (chr . (+ offset) . ord)
+
+decode :: Int -> String -> String
+decode shift = encode (negate shift)
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing
