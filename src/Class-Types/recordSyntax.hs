54ec0866e49ec1661d3765181057f2590efa0961
tykowale <tykowale@gmail.com>
2016-09-01 20:08:50 -0500

    deriving types

--- src/Class-Types/recordSyntax.hs
+++ src/Class-Types/recordSyntax.hs
@@ -1,9 +1,6 @@
 data Person = Person { firstName :: String
                      , lastName :: String
-                     , age :: Int
-                     , height :: Float
-                     , phoneNumber :: String
-                     , flavor :: String } deriving (Show)
+                     , age :: Int } deriving (Eq, Show, Read)
 
 data Car = Car { company :: String
                , model :: String
@@ -13,3 +10,14 @@ data Car = Car { company :: String
 tellCar :: Car -> String
 tellCar (Car {company = c, model = m, year =y }) =
   "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+mikeD = Person {firstName = "Michael", lastName = "Diamond", age = 43}
+adRock = Person {firstName = "Adam", lastName = "Rock", age = 41}
+mca = Person {firstName = "Adam", lastName = "Yauch", age = 44}
+
+mysteryDude = "Person { firstName =\"Michael\"" ++
+                     ", lastName =\"Diamond\"" ++
+                     ", age = 43}"
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+  deriving (Eq, Ord, Show, Read, Bounded, Enum)
