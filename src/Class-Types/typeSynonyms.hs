c7d6cc7e9e1972e22b6b0e58c1c85c41ccb97e18
tykowale <tykowale@gmail.com>
2016-09-13 20:42:35 -0500

    custom list

---
+++ src/Class-Types/typeSynonyms.hs
@@ -0,0 +1,32 @@
+import qualified Data.Map as Map
+
+-- type PhoneNumber = String
+-- type Name = String
+-- type PhoneBook = [(Name, Phonenumber)]
+
+-- inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+-- inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+-- inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+-- inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map = case Map.lookup lockerNumber map of
+                                  Nothing -> Left $ "Locker " ++ show lockerNumber ++ " doesn't exist!"
+                                  Just (state, code) -> if state /= Taken
+                                                           then Right code
+                                                           else Left $ "Locker " ++ show lockerNumber
+                                                             ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList [(100,(Taken, "ZD39I"))
+                       ,(101,(Free, "JAH3I"))
+                       ,(103,(Free, "IQSA9"))
+                       ,(105,(Free, "QOTSA"))
+                       ,(109,(Taken, "893JJ"))
+                       ,(110,(Taken, "99292"))
+                       ]
