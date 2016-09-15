f75c805efbde8780c0cb0c9ae62d5b99a16c8d99
tykowale <tykowale@gmail.com>
2016-09-14 20:56:13 -0500

    class stuff

--- src/Class-Types/recursive.hs
+++ src/Class-Types/recursive.hs
@@ -6,3 +6,34 @@ infixr 5 ^++
 Empty ^++ ys = ys
 (x :-: xs) ^++ ys = x :-: (xs ^++ ys)
 
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red light"
+  show Green = "Green light"
+  show Yellow = "Yellow light"
